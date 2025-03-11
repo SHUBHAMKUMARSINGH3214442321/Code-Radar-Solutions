27a91ce5646f07e64c5dc8c6272a23d518124102
SHUBHAMKUMARSINGH3214442321 <shubham2410991727@gmail.com>
2025-03-11 18:52:22 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
-int main(){
-    int a,b,c;
-    scanf("%d %d %d",&a ,&b ,&c);
-    if(a==b && a==c && b==c) printf("Equilateral");
-    if(a!=b && a!=c && b!=c) printf("Scalene");
-    else printf("Isosceles");
+
+// Function to check the type of the triangle
+void checkTriangleType(int a, int b, int c) {
+    // Check if the sides form a valid triangle
+    if (a + b > c && a + c > b && b + c > a) {
+        // Check if all sides are equal (Equilateral triangle)
+        if (a == b && b == c) {
+            printf("The triangle is Equilateral.\n");
+        }
+        // Check if two sides are equal (Isosceles triangle)
+        else if (a == b || b == c || a == c) {
+            printf("The triangle is Isosceles.\n");
+        }
+        // If no sides are equal (Scalene triangle)
+        else {
+            printf("The triangle is Scalene.\n");
+        }
+    } else {
+        printf("The given sides do not form a valid triangle.\n");
+    }
+}
+
+int main() {
+    int a, b, c;
+
+    // Get the input from the user
+    printf("Enter the lengths of the three sides of the triangle:\n");
+    printf("Side 1: ");
+    scanf("%d", &a);
+    printf("Side 2: ");
+    scanf("%d", &b);
+    printf("Side 3: ");
+    scanf("%d", &c);
+
+    // Check the type of triangle
+    checkTriangleType(a, b, c);
+
     return 0;
 }
