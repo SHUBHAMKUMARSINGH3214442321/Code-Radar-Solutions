773cd4fcd63ac5526bb8e59076f35140ed05389f
SHUBHAMKUMARSINGH3214442321 <shubham2410991727@gmail.com>
2025-03-11 18:59:30 +0530

    Solution to Check if a Number is Square of Another

--- Check if a Number is Square of Another@@6788dd63490637cfc44e1ba4/code.c
+++ Check if a Number is Square of Another@@6788dd63490637cfc44e1ba4/code.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
 int isPrime(int num) {
-    // Handle edge cases for numbers less than 2
     if (num <= 1) {
-        return 0; // Not prime
+        return 0; 
     }
 
-    // Check divisibility from 2 to the square root of num
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
-            return 0; // Not prime
+            return 0; 
         }
     }
-    return 1; // Prime
+    return 1; 
 }
 
 int main() {
     int number;
 
-    // Ask the user to input a number
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    // Check if the number is prime
     if (isPrime(number)) {
         printf("%d is a prime number.\n", number);
     } else {
